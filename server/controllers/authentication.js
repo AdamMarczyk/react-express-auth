124acfe008f774f98f525f87d5f63b66562e58e0
Adam Marczyk <adam.marczyk90@gmail.com>
2018-07-25 20:49:42 +0200

    create user record

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -4,6 +4,32 @@ exports.signup = function(req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
 
-  // See if a user with the given email exists
-  User.findOne({ email: email }, function(err, existingUser) {});
+  if (!email || !password) {
+    return res
+      .status(422)
+      .send({ error: 'You must provide email and password' });
+  }
+
+  User.findOne({ email: email }, function(err, existingUser) {
+    if (err) {
+      return next(err);
+    }
+
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email is in use' });
+    }
+
+    const user = new User({
+      email: email,
+      password: password
+    });
+
+    user.save(function(err) {
+      if (err) {
+        return next(err);
+      }
+
+      res.json({ success: true });
+    });
+  });
 };
