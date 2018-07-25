d15384dd7d5f7a2b20063955f779288c51b46f60
Adam Marczyk <adam.marczyk90@gmail.com>
2018-07-25 20:47:56 +0200

    add search for user

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,3 +1,9 @@
+const User = require('../models/user');
+
 exports.signup = function(req, res, next) {
-  res.send({ success: 'true' });
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // See if a user with the given email exists
+  User.findOne({ email: email }, function(err, existingUser) {});
 };
