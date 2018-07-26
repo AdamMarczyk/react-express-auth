bb4c2060aa4f9951b6aad72c215400f213282b7c
Adam Marczyk <adam.marczyk90@gmail.com>
2018-07-26 08:29:10 +0200

    remove

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -7,10 +7,6 @@ const INITIAL_STATE = {
 
 export default function(state = INITIAL_STATE, action) {
   switch (action.type) {
-    case AUTH_USER:
-      return { ...state, authenticated: action.payload };
-    case AUTH_ERROR:
-      return { ...state, errorMessage: action.payload };
     default:
       return state;
   }
