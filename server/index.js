8df4d53f6e70b69d58d73a9a4dba3f2eeffb7f69
Adam Marczyk <adam.marczyk90@gmail.com>
2018-07-26 08:39:21 +0200

    add cors

--- server/index.js
+++ server/index.js
@@ -3,12 +3,14 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
 const router = require('./router');
 
 mongoose.connect('mongodb://localhost/auth');
 
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
